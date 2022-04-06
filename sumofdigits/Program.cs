95e4d6499f31a13942c7706d2102eb9ea6ef0e5f
hp <hp@DESKTOP-NAHSNGU>
2022-04-06 22:24:14 +0530

    Add project files.

---
+++ sumofdigits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
